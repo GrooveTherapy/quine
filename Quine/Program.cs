9f61737b17d7d48e52a163c2766b257cb881f60a
Kent Brockman <KBrockman@matrix-solutions.com>
2015-05-20 19:35:17 -0600

    Initial commit

---
+++ Quine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Quine
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var dq = (char) 34;
+            var src = new[]
+                          {
+                              "using System.IO; using System.Text;", "namespace Quine", "{", "class Program", "{",
+                              "static void Main()", "{", "var dq = (char)34;", "var src = REPLACE",
+                              "var srcCombined = new StringBuilder(new string(new[]{'n','e','w','[',']',' ', '{', ' '}));",
+                              "foreach (var s in src) { srcCombined.Append(dq + s + dq + ',' + ' '); }",
+                              "srcCombined.Append(new string(new[]{'}',';'}));",
+                              "src[8] = src[8].Replace(new string(new[]{'R','E', 'P', 'L', 'A', 'C', 'E'}), srcCombined.ToString());"
+                              , "File.WriteAllLines(Directory.GetCurrentDirectory() + Path.GetRandomFileName(), src);", "}"
+                              , "}", "}",
+                          };
+            var srcCombined = new StringBuilder(new string(new[] {'n', 'e', 'w', '[', ']', ' ', '{', ' '}));
+            foreach (var s in src)
+            {
+                srcCombined.Append(dq + s + dq + ',' + ' ');
+            }
+            srcCombined.Append(new string(new[] {'}', ';'}));
+            src[8] = src[8].Replace(new string(new[] {'R', 'E', 'P', 'L', 'A', 'C', 'E'}), srcCombined.ToString());
+            File.WriteAllLines(Directory.GetCurrentDirectory() + Path.GetRandomFileName(), src);
+        }
+    }
+}
